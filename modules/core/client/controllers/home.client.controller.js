ee7d5a50d9d8bdd05472c352c3582195237b7f2d
Kwanthanna <thannarotk@gmail.com>
2025-05-28 14:28:18 +0700

    change logo

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,25 +5,25 @@
 		{
 			'alt': 'The United States Agency for International Development',
 			'url': 'https://www.usaid.gov/',
-			'src': 'https://servir.adpc.net/themes/svmk/images/optimized/USAID_Logo_Color.png',
+			'src': '/img/partners/usaid.png',
 			'className': 'usaid'
 		},
 		{
 			'alt': 'The National Aeronautics and Space Administration',
 			'url': 'https://www.nasa.gov/',
-			'src': 'https://servir.adpc.net/themes/svmk/images/optimized/NASA_Logo_Color.png',
+			'src': '/img/partners/nasa.png',
 			'className': 'nasa'
 		},
 		{
 			'alt': 'Asian Disaster Preparedness Center',
 			'url': 'http://www.adpc.net/',
-			'src': 'https://servir.adpc.net/themes/svmk/images/optimized/partner-adbc.png',
+			'src': '/img/partners/adpc.png',
 			'className': 'adpc'
 		},
 		{
 			'alt': 'SERVIR',
 			'url': 'https://servir.adpc.net/',
-			'src': 'https://servir.adpc.net/themes/svmk/images/optimized/Servir_Logo_Color.png',
+			'src': '/img/partners/servir.png',
 			'className': 'servir'
 		}
 	];
